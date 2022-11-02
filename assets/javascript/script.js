6026fb06a61460d778a4988e3f1ba0d05f6dd90c
Mouadh <mouad-fe@outlook.com>
2022-11-02 20:07:18 +0100

    28-commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,3 +10,25 @@ setInterval(() => {
     number.innerHTML = counter + "%";
   }
 }, 30);
+
+let number1 = document.getElementById("number1");
+let counter1 = 0;
+setInterval(() => {
+  if (counter1 == 85) {
+    clearInerval();
+  } else {
+    counter1 += 1;
+    number1.innerHTML = counter1 + "%";
+  }
+}, 30);
+
+let number2 = document.getElementById("number2");
+let counter2 = 0;
+setInterval(() => {
+  if (counter2 == 90) {
+    clearInerval();
+  } else {
+    counter2 += 1;
+    number2.innerHTML = counter2 + "%";
+  }
+}, 30);
